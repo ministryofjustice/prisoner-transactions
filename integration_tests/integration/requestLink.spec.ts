6eacbe800af1f412348ac3de61c15d27942feed5
Mike Halma <mike.halma@digital.justice.gov.uk>
2021-09-27 18:02:35 +0100

    314 Remove unnecessary tests

--- integration_tests/integration/requestLink.spec.ts
+++ integration_tests/integration/requestLink.spec.ts
@@ -13,50 +13,35 @@ context('Request Link Page', () => {
     cy.task('stubRequestLink')
   })
 
-  describe('layout', () => {
-    it('Can access request Link page without a login', () => {
-      cy.visit('/request-link')
-      Page.verifyOnPage(RequestLinkPage)
-    })
-
-    it('Has an email input field', () => {
-      cy.visit('/request-link')
-      const requestLinkPage = Page.verifyOnPage(RequestLinkPage)
-      requestLinkPage.email().should('exist')
-    })
-
-    it('Has a request link button', () => {
-      cy.visit('/request-link')
-      const requestLinkPage = Page.verifyOnPage(RequestLinkPage)
-      requestLinkPage.requestButton().should('exist')
-    })
+  it('Can access request Link page without a login', () => {
+    cy.visit('/request-link')
+    const requestLinkPage = Page.verifyOnPage(RequestLinkPage)
+    requestLinkPage.email().should('exist')
+    requestLinkPage.requestButton().should('exist')
   })
 
-  describe('submit email', () => {
-    it('should show email sent page for a valid email', () => {
-      cy.visit('/request-link')
-      const requestLinkPage = Page.verifyOnPage(RequestLinkPage)
-      requestLinkPage.email().type('amy.barnett@digital.justice.gov.uk')
-      requestLinkPage
-        .requestButton()
-        .click()
-        .then(requestLinkRequests)
-        .then(requests => {
-          expect(requests).to.have.lengthOf(1)
-          expect(requests[0].request.url).to.equal(
-            '/prisoner-transactions/link/email/amy.barnett@digital.justice.gov.uk'
-          )
-          Page.verifyOnPage(EmailSentPage)
-        })
-    })
-    it('should show errors for an invalid email', () => {
-      cy.visit('/request-link')
-      const requestLinkPage = Page.verifyOnPage(RequestLinkPage)
-      requestLinkPage.email().type('invalid-email')
-      requestLinkPage.requestButton().click()
-      const requestLinkPageResult = Page.verifyOnPage(RequestLinkPage)
-      requestLinkPageResult.errorSummary().contains('Enter a valid email address')
-    })
+  it('should show email sent page for a valid email', () => {
+    cy.visit('/request-link')
+    const requestLinkPage = Page.verifyOnPage(RequestLinkPage)
+    requestLinkPage.email().type('amy.barnett@digital.justice.gov.uk')
+    requestLinkPage
+      .requestButton()
+      .click()
+      .then(requestLinkRequests)
+      .then(requests => {
+        expect(requests).to.have.lengthOf(1)
+        expect(requests[0].request.url).to.equal('/prisoner-transactions/link/email/amy.barnett@digital.justice.gov.uk')
+        Page.verifyOnPage(EmailSentPage)
+      })
+  })
+
+  it('should show errors for an invalid email', () => {
+    cy.visit('/request-link')
+    const requestLinkPage = Page.verifyOnPage(RequestLinkPage)
+    requestLinkPage.email().type('invalid-email')
+    requestLinkPage.requestButton().click()
+    const requestLinkPageResult = Page.verifyOnPage(RequestLinkPage)
+    requestLinkPageResult.errorSummary().contains('Enter a valid email address')
   })
 })
 
@@ -64,7 +49,6 @@ const getRequestsFor = (filter: (request: WireMockRequest) => boolean) =>
   getRequests().then((response: { body: AllWireMockRequest }) => response.body.requests.filter(filter))
 
 const isRequestLinkRequest = (request: WireMockRequest) =>
-  // request.request.url.match('/link/email/.*') && request.request.method === 'POST'
-  request.request.url.match('.*') && true
+  request.request.url.match('/prisoner-transactions/link/email/.*') && request.request.method === 'POST'
 
 const requestLinkRequests = () => getRequestsFor(isRequestLinkRequest)
